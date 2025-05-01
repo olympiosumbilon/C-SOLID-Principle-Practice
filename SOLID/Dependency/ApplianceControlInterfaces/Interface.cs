da9cbe398453eec7c79d79b4897f27b93b281969
Sumbilon-817 <olympiosumbilon17@gmail.com>
2025-05-01 20:15:44 +0800

    Not Yet Working Need to find a solution

---
+++ SOLID/Dependency/ApplianceControlInterfaces/Interface.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID_Principle_Practice.SOLID.Dependency.ApplianceControlInterfaces
+{
+    public interface IAppliance
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+
+    public class LightControl : IAppliance{
+        public void TurnOn() => Console.WriteLine("Turning on the light");
+        public void TurnOff() => Console.WriteLine("Turning off the light");
+    }
+
+    public class ACControl : IAppliance {
+        public void TurnOn() => Console.WriteLine("Turning on the air conditioner");
+        public void TurnOff() => Console.WriteLine("Turning off the air conditioner");
+    }
+}
---
+++ SOLID/SRP/AppliancesControllers/TimerControl.cs
@@ -0,0 +1,7 @@
+﻿namespace SOLID_Principle_Practice.SOLID.SRP.AppliancesControllers
+{
+    public class TimerControl
+    {
+        public void SetTimer() => Console.WriteLine("Setting timer for appliances");
+    }
+}
